7eb166851c8081a10f9ef1af2b32433aba106107
pjeon18 <102201369+pjeon18@users.noreply.github.com>
2024-10-09 11:15:31 -0400

    ww
    ww

--- fall2024/psets/ps3/experiments.py
+++ fall2024/psets/ps3/experiments.py
@@ -3,7 +3,6 @@
 # Run `python experiments.py` to generate the plot.
 
 import simulator
-
 import matplotlib.pyplot as plt
 import time
 
@@ -35,6 +34,10 @@ prog1 = [8,
     ['assign', output_len_id, 1], 
     ['assign', output_ptr_id, 0],
     # TODO: lines 5-8 from pseudocode
+    ['assign', result_id, 13],
+    ['read', counter_id, zero_id],
+    ['goto', counter_id, 11],
+    ['*', result_id, result_id, result_id],
     ['-', counter_id, counter_id, one_id],
     ['goto', zero_id, 7],
     ['*', result_id, result_id, thirteen_id],
@@ -57,6 +60,12 @@ prog2 = [10,
     ['*', temp_id, temp_id, W_id],
     ['-', result_id, result_id, temp_id],
     ['-', counter_id, counter_id, one_id],
+    ['goto', zero_id, 8],
+    ['*', result_id, result_id, thirteen_id],
+    ['/', temp_id, result_id, W_id],
+    ['*', temp_id, temp_id, W_id],
+    ['-', result_id, result_id, temp_id],
+    ['write', output_ptr_id, result_id]
     # TODO: lines 14-19 from pseudocode
 ]
 
